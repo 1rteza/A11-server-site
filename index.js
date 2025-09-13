08518d511678efda50e3c5aee0a3fc9094a49aa3
Irteza <irtezarashidnai@gmail.com>
2025-09-14 02:37:35 +0600

    created bookings route

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ async function run() {
     })
 
 
+    app.post('/bookings', async(req, res) => {
+      const booking = req.body;
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
